95e358d279959f8165f38e3422bc6069ba322629
Steve Vissault <s.vissault@yahoo.fr>
2016-12-23 12:40:24 -0500

    Fixing issue with branches

--- _deploy.sh
+++ _deploy.sh
@@ -13,13 +13,14 @@ git clone https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git slides
 cd slides
 git checkout --orphan gh-pages
 git rm -rf .
-
+git checkout master
 
 for dir in */ ; do
     cd $dir
-    git checkout master -- ./pres/assets/ ./pres/libraries/ ./pres/index.html
+    git checkout gh-pages -- ./pres/assets/ ./pres/libraries/ ./pres/index.html
     cd ..
 done
 
+git checkout gh-pages
 git commit -m "Update the slides" || true
 git push origin gh-pages
