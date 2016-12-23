d90e3fa8574a3404e541434f1a952ede7d6ae4aa
Steve Vissault <s.vissault@yahoo.fr>
2016-12-23 13:10:43 -0500

    Fixing Travis

--- _deploy.sh
+++ _deploy.sh
@@ -8,19 +8,16 @@ set -e
 git config --global user.email "s.vissault@yahoo.fr"
 git config --global user.name "SteveViss"
 
-git clone https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git slides
-
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git slides
 cd slides
-git checkout --orphan gh-pages
-git rm -rf .
-git checkout master
+git reset --hard origin/master
 
 for dir in */ ; do
     cd $dir
-    git checkout gh-pages -- ./pres/assets/ ./pres/libraries/ ./pres/index.html
+    cp -r ./pres/* .
+    git add ./assets/ ./libraries/ ./index.html
     cd ..
 done
 
-git checkout gh-pages
 git commit -m "Update the slides" || true
 git push origin gh-pages
