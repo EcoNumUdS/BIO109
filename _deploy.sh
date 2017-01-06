965b2dba6cc92cbc86c57be46bbd113897b0fd2d
Steve Vissault <s.vissault@yahoo.fr>
2017-01-06 17:53:38 -0500

    fix travis deployment

--- _deploy.sh
+++ _deploy.sh
@@ -9,6 +9,7 @@ git config --global user.email "s.vissault@yahoo.fr"
 git config --global user.name "SteveViss"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git slides
+rm -rf slides/*
 
 for dir in $(ls -d cours*); do
     mkdir -p ./slides/$dir
