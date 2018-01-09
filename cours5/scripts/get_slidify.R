516db448298920049c82a5b0bc623aed7ab036fa
Guillaume Blanchet <guiblanchet3@gmail.com>
2018-01-09 14:22:17 -0500

    Travis encore

--- cours5/scripts/get_slidify.R
+++ cours5/scripts/get_slidify.R
@@ -1,12 +1,10 @@
 ## Installation package slidify if not installed
 if (!require(devtools)) install.packages("devtools")
 if (!require(slidify)){
-  devtools::install_github("ramnathv/slidify")
-
   deps <- c("knitr", "markdown", "yaml", "whisker", "stringr")
   install.packages(deps)
+  devtools::install_github("ramnathv/slidifyLibraries")
+  devtools::install_github("ramnathv/slidify")
 }
-if (!require(slidifyLibraries)) devtools::install_github("ramnathv/slidifyLibraries")
-if (!require(rCharts)) devtools::install_github("ramnathv/rCharts")
 
 slidify::slidify('./pres/index.Rmd')
