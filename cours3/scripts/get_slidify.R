dcd91fb7e079eb5744edc5c383e8cec83bee5496
Guillaume Blanchet <guiblanchet3@gmail.com>
2018-01-09 14:10:35 -0500

    Ajout des dépendence à slidify sur Travis

--- cours3/scripts/get_slidify.R
+++ cours3/scripts/get_slidify.R
@@ -1,5 +1,12 @@
-
 ## Installation package slidify if not installed
-if (!require(slidify)) devtools::install_github("ramnathv/slidify")
+if (!require(devtools)) install.packages("devtools")
+if (!require(slidify)){
+  devtools::install_github("ramnathv/slidify")
+
+  deps <- c("knitr", "markdown", "yaml", "whisker", "stringr")
+  install.packages(deps)
+}
 if (!require(slidifyLibraries)) devtools::install_github("ramnathv/slidifyLibraries")
+if (!require(rCharts)) devtools::install_github("ramnathv/rCharts")
+
 slidify::slidify('./pres/index.Rmd')
