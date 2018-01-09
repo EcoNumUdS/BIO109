5926f78443466ef14feef1b6b278e85f405a79a5
Guillaume Blanchet <guiblanchet3@gmail.com>
2018-01-09 14:47:40 -0500

    Travis encore (3)

--- cours4/scripts/get_slidify.R
+++ cours4/scripts/get_slidify.R
@@ -1,12 +1,6 @@
+
 ## Installation package slidify if not installed
 if (!require(devtools)) install.packages("devtools")
-
-install.packages("knitr")
-install.packages("markdown")
-install.packages("yaml")
-install.packages("whisker")
-install.packages("stringr")
-devtools::install_github("ramnathv/slidifyLibraries")
-devtools::install_github("ramnathv/slidify")
-
+if (!require(slidify)) devtools::install_github("ramnathv/slidify")
+if (!require(slidifyLibraries)) devtools::install_github("ramnathv/slidifyLibraries")
 slidify::slidify('./pres/index.Rmd')
